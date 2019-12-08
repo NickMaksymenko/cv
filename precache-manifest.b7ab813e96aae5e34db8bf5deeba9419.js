f9ee70ac65d3684077d5362abb75f9cd355a3edc
Nikolay Maksymenko <>
2019-12-08 16:14:30 +0200

    Updates

---
+++ precache-manifest.b7ab813e96aae5e34db8bf5deeba9419.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168cb2cf650f49024268c91938efebd6",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "5afd25414da7a6ba9c08",
+    "url": "/cv/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "90f72440293a89f37fef",
+    "url": "/cv/static/js/2.c4318512.chunk.js"
+  },
+  {
+    "revision": "6ed4837c9741c870d03db3db11c80b52",
+    "url": "/cv/static/js/2.c4318512.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5afd25414da7a6ba9c08",
+    "url": "/cv/static/js/main.5d07565f.chunk.js"
+  },
+  {
+    "revision": "0be9b0cd05063b42ef72",
+    "url": "/cv/static/js/runtime-main.e1e8508d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
